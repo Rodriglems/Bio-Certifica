8d61a133a0593de3392d5b6b10be52431c572588
Rodrigo Lemos <rcamilodelemos@gmail.com>
2026-05-27 11:11:24 -0300

    fix: atualizar nome e ID do aplicativo para Bio Certifica

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'br.com.agroecologia.app',
-  appName: 'App Agroecologia',
+  appId: 'br.com.biocertifica.app',
+  appName: 'Bio Certifica',
   webDir: 'build'
 };
 
